05c9a6edf36c168659f3edbee99159724123f705
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-05-22 20:10:30 +0000

    May 23, 2023, 2:10 AM

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -13,16 +13,19 @@ import { mapState } from 'vuex';
 export default {
     computed: mapState(['board'])
 }
+
 </script>
-  
-  <style lang="css">
-  .board {
-    @apply p-4 bg-teal-dark h-full overflow-auto;
-  }
-  
-  .task-bg {
-    @apply pin absolute;
-    background: rgba(0,0,0,0.5);
-  }
-  </style>
+<style lang="css">
+    .board {
+        @apply p-4 bg-teal-dark h-full overflow-auto;
+    }
+    .column {
+        @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+        min-width: 350px;
+    }
+    .task-bg {
+        @apply pin absolute;
+        background: rgba(0,0,0,0.5);
+    }
+</style>
   
