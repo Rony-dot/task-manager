b880dd4eeeb1df0b66c04531f870bb16ecc1a81a
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-05-22 19:56:41 +0000

    May 23, 2023, 1:56 AM

---
+++ src/views/Board.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="board">
+      
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    
+  }
+  </script>
+  
+  <style lang="css">
+  .board {
+    @apply p-4 bg-teal-dark h-full overflow-auto;
+  }
+  
+  .task-bg {
+    @apply pin absolute;
+    background: rgba(0,0,0,0.5);
+  }
+  </style>
+  
