cfb9606a97fa15c2fcfe228770cdcf53540a6822
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-05-23 20:14:02 +0000

    May 24, 2023, 2:14 AM

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from "vue";
 import '../index.css';
-import 'tailwindcss/dist/tailwind.css';
-
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
-    content: [],
-    theme: {},
+    content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend:{
+            colors:{
+
+            }
+        },
+        fontFamily:{
+
+        },
+        container:{
+
+        },
+        screens:{
+            
+        }
+    },
     variants: {},
     plugins: [],
   }
