293ac0a7c30b92fcc2482e13395b84a8622f5519
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-05-23 20:00:10 +0000

    May 24, 2023, 2:00 AM

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import '../index.css';
+import 'tailwindcss/dist/tailwind.css';
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
