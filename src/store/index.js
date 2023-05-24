316ee955d062d2cea889489b376c5b80cc1be511
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-05-24 18:24:52 +0000

    May 25, 2023, 12:24 AM

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 import { createStore } from "vuex";
 import defaultBoard from "@/default-board";
+import { saveStatePlugin } from "@/utils";
 
-// const board = defaultBoard
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
 
 export default createStore({
+  plugins : [saveStatePlugin],
   state: {
-    board: defaultBoard
+    // board: board // redundant that's why we will use the below  
+     board
   },
   getters: {},
   mutations: {},
